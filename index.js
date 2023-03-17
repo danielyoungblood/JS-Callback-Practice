f8be2d49eb42f0a7db31ddc30c1e2c9b34e2c9be
daniel <danielyknechtli@gmail.com>
2023-03-17 16:15:46 -0700

    finishing up changes

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const inventory = newInventory();
 move(inventory).to(0, 0);
 
 const character = newImage("assets/green-character/static.gif");
-move(character).withArrowKeys(100, 250);
+
+//this function is being passed to the withArrowKeys function to change the image of the character as its moving
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange); //we refer to handleDirectionChange function as the name callback in move.js where withArrowKeys function is defined
 
 move(newImage("assets/tree.png")).to(200, 450);
 move(newImage("assets/pillar.png")).to(350, 250);
--- move.js
+++ move.js
@@ -7,27 +7,46 @@ function move(element) {
   }
 
   //this will handle moving the character with the character keys
-  function moveWithArrowKeys(left, bottom) {
+  function moveWithArrowKeys(left, bottom, callback) {
+    let direction = null;
     let x = left;
     let y = bottom;
-    element.style.left = x + "px";
-    element.style.bottom = y + "px";
 
     //took code from the moveCharacter function and used it directly in setInterval function, this is referred to as inline function
     setInterval(function () {
-      let direction = null;
+      console.log("moveWithArrowKeys - setInterval, direction: " + direction);
+
+      //in lines 23-34, we are changing the value of variables x or y depending on the value of the variable direction
       if (direction === "west") {
-        x = x - 1;
+        //if the direction variable contains the value "west"
+        x = x - 1; //then set the variable of the value x to the current value of x-1
       }
       if (direction === "north") {
-        y = y + 1;
+        //if the direction variable contains the value "north"
+        y = y + 1; //then set the variable of the value y to the current value of y+1
       }
       if (direction === "east") {
-        x = x + 1;
+        //if the direction variable contains the value "east"
+        x = x + 1; //then set the variable of the value x to the current value of x+1
       }
       if (direction === "south") {
-        y = y - 1;
+        //if the direction variable contains the value "south"
+        y = y - 1; //then set the variable of the value y to the current value of y-1
       }
+
+      element.style.left = x + "px"; //this determines where the element is on the screen according to its x postion
+      element.style.bottom = y + "px"; //this deteremins where the element is on the screen according to its y postion
+
+      //console.log() is a Javascript function that already exists in Javascript, what is custom and changes is what is
+      //between the () which gets printed to the console window in the browser
+      console.log("moveWithArrowKeys - setInterval: x= " + x + ", y= " + y);
+
+      console.log(
+        "moveWithArrowKeys - setInterval: element.style.left=" +
+          element.style.left +
+          ", element.style.bottom=" +
+          element.style.bottom
+      );
     }, 1);
 
     //we are adding an inline function to handle the event when either the left arrow key, right arrow key, up arrow key, or down arrow key is pressed
@@ -51,11 +70,15 @@ function move(element) {
       if (keyBoardEventParameter.key === "ArrowDown") {
         direction = "south";
       }
+      callback(direction);
     });
+
+    console.log("document.addEventListene, direction: " + direction);
 
     //adding one more event listener to stop the character when the user releases a key
     document.addEventListener("keyup", function (keyBoardEventParameter) {
       direction = null;
+      callback(direction);
     });
   }
 
